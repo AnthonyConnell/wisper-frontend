bb0b520e168b115c441a4bf5320fb2237dd4d914
Anthony Connell <toco85@gmail.com>
2022-07-19 00:51:27 -0400

    Wrote code for the barebones of the UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import wisper_placeholder from './images/wisper_placeholder.png';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Wisper</Typography>
+        <img src={wisper_placeholder} alt="wisper logo" height="450" /> 
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing="3">
+            <Grid item xs={12} sm={7}>
+                <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
