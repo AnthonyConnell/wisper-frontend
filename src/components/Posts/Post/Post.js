67cd1676de8a559f855e229c7a939ff257cdf2b8
Anthony Connell <toco85@gmail.com>
2022-07-20 19:46:29 -0400

    Adding Redux capabilities as well as the posts ui and preparing for api

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+// import wisper from 'wisper';
+import useStyles from './styles';
 
-const Post = () => {
+const Post = ({ post }) => {
+    const classes = useStyles();
+    
     return (
         <h1>POST</h1>
     );
