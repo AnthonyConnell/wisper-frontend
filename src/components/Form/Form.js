8e055e5a17ab7b7adcd082e4f5b059a699a8d12a
Anthony Connell <toco85@gmail.com>
2022-07-23 11:50:11 -0400

    adding node version to package.json for heroku deployment

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -36,7 +36,7 @@ const Form = ({ currentId, setCurrentId }) => {
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
+        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Writing a Wisp'}</Typography>
         <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
+
 
 import Post from './Post/Post';
 import useStyles from './styles';
@@ -8,6 +9,14 @@ import useStyles from './styles';
 const Posts = ({setCurrentId}) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+    const [user, setUser] = useState() 
+
+    useEffect(async () => {
+        var posts = await fetch("https://wisper-api-71822.herokuapp.com/user/1?withPosts=true");
+        console.log(posts)
+        .then(response => response.json())
+        .then(data => setUser(data.user_id))
+    })
 
     return (
         !Posts.length ? <CircularProgress /> : (
