f8567b5ab0202c62e2132936effab60ad6aace2d
Werefox <jack@wrightus.com>
2022-07-24 16:23:24 -0400

    Fixed the ReactDOM.render warning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -10,9 +10,10 @@ import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
