3d27fcf63dde0557845cfffd457307d4e9c4d1d0
Werefox <jack@wrightus.com>
2022-07-24 16:39:15 -0400

    Changed _id to post_id in Posts.js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -25,7 +25,7 @@ const Posts = ({setCurrentId}) => {
         !userPosts ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3} >
                 {userPosts.posts.map((post) => (
-                    <Grid key={post._id} item xs={12} sm={6}>
+                    <Grid key={post.post_id} item xs={12} sm={6}>
                         <Post post={post} />
                     </Grid>
                 ))}
