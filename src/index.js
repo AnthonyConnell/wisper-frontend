a714015bcd63f6f6789238fc411ddd1bd58b1688
Werefox <jack@wrightus.com>
2022-07-24 16:29:46 -0400

    Changed createStore to legacy_createStore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { legacy_createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './App';
 import reducers from './reducers';
 import './index.css';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)))
 
 const container = document.getElementById('root')
 const root = createRoot(container)
